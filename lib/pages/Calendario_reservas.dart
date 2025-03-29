f4e9dca8344e56774b0b66c4ea31201cdab06b8a
Andrik4 <22300257@uttt.edu.mx>
2025-03-29 17:49:55 -0600

    Actualización de archivos y nuevas páginas

---
+++ lib/pages/Calendario_reservas.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:restaurante/pages/payment_page.dart'; // Ajusta la ruta si es necesario
+
+class ReservaScreen extends StatefulWidget {
+  @override
+  _ReservaScreenState createState() => _ReservaScreenState();
+}
+
+class _ReservaScreenState extends State<ReservaScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  final List<String> _reservas = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Selección de Reserva')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2025, 1, 1),
+              lastDay: DateTime.utc(2025, 12, 31),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              },
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_selectedDay != null) {
+                      final result = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder:
+                              (context) =>
+                                  PaymentPage(selectedDate: _selectedDay!),
+                        ),
+                      );
+
+                      if (result == true) {
+                        _agendarReserva(_selectedDay!);
+                      }
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Por favor selecciona una fecha'),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('Proceder al Pago'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_selectedDay != null) {
+                      _cancelarReserva(_selectedDay!);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Por favor selecciona una fecha'),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('Cancelar Reserva'),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Mis Reservas',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _reservas.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Reserva el ${_reservas[index]}'),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _agendarReserva(DateTime fecha) {
+    setState(() {
+      _reservas.add('${fecha.day}/${fecha.month}/${fecha.year}');
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          'Reserva agendada para el ${fecha.day}/${fecha.month}/${fecha.year}',
+        ),
+      ),
+    );
+  }
+
+  void _cancelarReserva(DateTime fecha) {
+    setState(() {
+      _reservas.remove('${fecha.day}/${fecha.month}/${fecha.year}');
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          'Reserva cancelada para el ${fecha.day}/${fecha.month}/${fecha.year}',
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SitEatApp());
+}
+
+class SitEatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0; // Índice de la barra inferior
+  String searchQuery = ""; // Texto de búsqueda
+
+  final List<Map<String, String>> products = [
+    {
+      'name': 'Tacos al Pastor',
+      'price': '\$18.00',
+      'image': 'assets/TacosPastor.jpg',
+      'description':
+          'Deliciosos tacos de cerdo marinado con chiles y especias, cocinados en trompo, servidos con piña, cebolla y cilantro.',
+    },
+    {
+      'name': 'Chiles en Nogada',
+      'price': '\$25.00',
+      'image': 'assets/ChilesNogada.jpg',
+      'description':
+          'Chiles poblanos rellenos de picadillo, bañados en salsa de nuez y decorados con granada y perejil.',
+    },
+    {
+      'name': 'Pozole Rojo',
+      'price': '\$20.00',
+      'image': 'assets/Pozole.jpg',
+      'description':
+          'Sopa espesa de maíz con carne de cerdo, cocida con chiles secos. Se sirve con rábanos, lechuga y tostadas.',
+    },
+    {
+      'name': 'Tamales',
+      'price': '\$15.00',
+      'image': 'assets/Tamales.jpg',
+      'description':
+          'Masa de maíz rellena de guisos como salsa roja, verde o dulce, envuelta en hoja de maíz y cocida al vapor.',
+    },
+    {
+      'name': 'Mole Poblano',
+      'price': '\$22.00',
+      'image': 'assets/Mole.jpg',
+      'description':
+          'Salsa espesa de chiles y chocolate que cubre pollo o guajolote. Se acompaña con arroz y tortillas.',
+    },
+    {
+      'name': 'Enchiladas Verdes',
+      'price': '\$19.00',
+      'image': 'assets/Enchiladas.jpg',
+      'description':
+          'Tortillas rellenas de pollo, bañadas en salsa de tomate verde con crema y queso, servidas con frijoles refritos.',
+    },
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, String>> filteredProducts =
+        products
+            .where(
+              (product) => product['name']!.toLowerCase().contains(
+                searchQuery.toLowerCase(),
+              ),
+            )
+            .toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Sit&Eat'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle), // Ícono de perfil
+            onPressed: () {
+              // Acción para ir al perfil
+            },
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Campo de búsqueda
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  searchQuery = value;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: 'Buscar productos...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Todos los productos',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, // 3 columnas
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                  childAspectRatio: 1.4, // Ajuste de altura de las tarjetas
+                ),
+                itemCount: filteredProducts.length,
+                itemBuilder: (context, index) {
+                  return ProductCard(
+                    name: filteredProducts[index]['name']!,
+                    price: filteredProducts[index]['price']!,
+                    image: filteredProducts[index]['image']!,
+                    description: filteredProducts[index]['description']!,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendario',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.payment), label: 'Pago'),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  final String name;
+  final String price;
+  final String image;
+  final String description;
+
+  const ProductCard({
+    required this.name,
+    required this.price,
+    required this.image,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      elevation: 3,
+      child: Column(
+        children: [
+          Expanded(
+            flex: 2, // Espacio de la imagen ajustado
+            child: ClipRRect(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
+              child: Image.asset(
+                image,
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0), // Menos padding interno
+            child: Column(
+              children: [
+                Text(name, style: TextStyle(fontWeight: FontWeight.bold)),
+                Text(price, style: TextStyle(color: Colors.green)),
+                Text(
+                  description,
+                  textAlign: TextAlign.center,
+                  maxLines: 2, // Limita a 2 líneas
+                  overflow: TextOverflow.ellipsis, // "..." si el texto es largo
+                  style: TextStyle(fontSize: 10), // Fuente más pequeña
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
